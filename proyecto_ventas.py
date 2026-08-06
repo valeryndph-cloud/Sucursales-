bf1c71ac7e54c59f9c90b7daeb2b15ee2aa4552e
valeryndph-cloud <valeryndph@gmail.com>
2026-08-06 13:33:26 -0500

    codigo inicial

---
+++ proyecto_ventas.py
@@ -0,0 +1,63 @@
+import pandas as pd 
+import glob 
+# Exploracion de los datos y diferentes tipos de archivos 
+#.csv y .xlsx
+
+df_medellin = pd.read_csv("sucursal_medellin.csv")
+#print(df_medellin.head(3))
+
+df_bogota = pd.read_excel("sucursal_bogota.xlsx")
+#print(df_bogota.head(3))
+
+#print(df_bogota.columns)
+#print(df_medellin.columns)
+
+#Agrupar archivos por tipo .csv y xlsx
+archivos_csv = glob.glob("*.csv")
+archivos_xlsx = glob.glob("*.xlsx")
+
+#print (archivos_csv)
+
+
+for archivo in archivos_csv:
+    df = pd.read_csv(archivo)
+    print(f"\n{archivo}")
+    print(df.columns.tolist())
+
+for archivo in archivos_xlsx:
+    df = pd.read_excel(archivo)
+    print(f"\n{archivo}")
+    print(df.columns.tolist())
+
+
+
+lista_informes = []
+
+for archivo in archivos_csv:
+    df = pd.read_csv(archivo)
+    lista_informes.append(df)
+    print(f"Leidos: {archivo} - {len(df)}")
+
+for archivo in archivos_xlsx:
+    df = pd.read_excel(archivo)
+    lista_informes.append(df)
+    print(f"Leidos: {archivo} - {len(df)}")
+
+
+ # Unificar todos los informes en un solo DataFrame
+for i, df in enumerate(lista_informes):
+    if'Fecha_Venta' in df.columns:
+        lista_informes[i] = df.rename(columns={
+        "Fecha_Venta" :"fecha","Producto": "producto",
+        "Categoria": "categoria", "Cant": "cantidad",
+        "Valor_Unitario": "precio_unitario",
+        "Vendedor": "vendedor", "Pago":"metodo_pago"
+        })
+
+df_consolidado = pd.concat(lista_informes, ignore_index=True)
+print(df_consolidado)
+print(f"Total de registros: {len(df_consolidado)}")
+#df_consolidado = df_consolidado.to_csv("Informe_sucursales", index = False)
+
+        
+
